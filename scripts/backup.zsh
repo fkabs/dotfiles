6b99dea10307711a10904bea8aab845effe32384
FKabs <fkabs@pm.me>
2023-11-04 16:30:54 +0100

    [backup] dock-layout is also saved from now on

--- scripts/backup.zsh
+++ scripts/backup.zsh
@@ -4,4 +4,10 @@
 DOTFILES=$HOME/.dotfiles
 
 # Make a brew dump file
+echo "\n# Dumping Brewfile"
 brew bundle dump --force --file=$DOTFILES/brew/Brewfile
+echo "Dumped file to $DOTFILES/brew/Brewfile"
+
+# Exporting dock layout
+echo "# Exporting dock layout to $DOTFILES/dock/dock-layout.plist"
+defaults export com.apple.dock $DOTFILES/dock/dock-layout.plist
