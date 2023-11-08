2a538080c6fd21e1db24f9ba3636b3f3f9460aa8
FKabs <fkabs@pm.me>
2023-11-08 21:32:58 +0100

    [zplugins] zsh-users syntax highlighting

--- zsh/.zplugins.zsh
+++ zsh/.zplugins.zsh
@@ -1,14 +1,14 @@
 fpath+=( /Users/fkabs/.antidote/romkatv/powerlevel10k )
+fpath+=( /Users/fkabs/.antidote/agkozak/zsh-z )
+source /Users/fkabs/.antidote/agkozak/zsh-z/zsh-z.plugin.zsh
 if ! (( $+functions[zsh-defer] )); then
   fpath+=( /Users/fkabs/.antidote/romkatv/zsh-defer )
   source /Users/fkabs/.antidote/romkatv/zsh-defer/zsh-defer.plugin.zsh
 fi
-fpath+=( /Users/fkabs/.antidote/zdharma-continuum/fast-syntax-highlighting )
-zsh-defer source /Users/fkabs/.antidote/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /Users/fkabs/.antidote/agkozak/zsh-z )
-source /Users/fkabs/.antidote/agkozak/zsh-z/zsh-z.plugin.zsh
 fpath+=( /Users/fkabs/.antidote/zsh-users/zsh-autosuggestions )
 zsh-defer source /Users/fkabs/.antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /Users/fkabs/.antidote/zsh-users/zsh-completions/src )
 fpath+=( /Users/fkabs/.antidote/zsh-users/zsh-history-substring-search )
 zsh-defer source /Users/fkabs/.antidote/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /Users/fkabs/.antidote/zsh-users/zsh-syntax-highlighting )
+zsh-defer source /Users/fkabs/.antidote/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
